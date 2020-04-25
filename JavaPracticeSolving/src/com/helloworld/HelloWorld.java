470508c8f89e5fe565a78e5e0235364b924d604c
karshilsheth <karshilsheth@gmail.com>
2020-04-25 22:02:29 +0530

    First Commit Using Eclispe. Java Program!

---
+++ JavaPracticeSolving/src/com/helloworld/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * 			Author			: 	Karshil	Sheth
+ * 			Date			:	25th April,2020
+ * 			Task			: Writing first program hello world. Pushing to git as well.
+ * 			
+ * 
+ * 
+ * 
+ * */
+package com.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+		System.out.println("Hello Java");
+	}
+}
