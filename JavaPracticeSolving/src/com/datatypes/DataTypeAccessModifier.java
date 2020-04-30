b134a7849a207de7ee2f09c2cabf0ad12ffea8eb
karshilsheth <karshilsheth@gmail.com>
2020-04-30 22:32:56 +0530

    Practiced on Access Specifier and Data Types Problem!

---
+++ JavaPracticeSolving/src/com/datatypes/DataTypeAccessModifier.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 				1) Final Methods Cannot be overriden
+ * 				2) 
+ * */
+
+
+package com.datatypes;
+
+public class DataTypeAccessModifier extends Subclass {
+
+	
+	public static void main(String args[]) {
+	
+		SuperClass sc=new Subclass();
+		sc.showSample();
+	
+	
+		Subclass fv=new Subclass();
+		fv.finalClass();
+		fv.showSample();
+	}
+}
+
+
+ class SuperClass{
+	private int a=5;
+	final long b=567l;
+	protected final void showSample() {
+		
+		System.out.println("Super Class with Protected method+"+" "+a+" "+b);
+	}
+}
+ 
+ class Subclass extends SuperClass{
+	
+	  void finalClass() {
+		   int a=45;
+		 System.out.println( "SubClass default Method"+a);
+	 }
+ }
