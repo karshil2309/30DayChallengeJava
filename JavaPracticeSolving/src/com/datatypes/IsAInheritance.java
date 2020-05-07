66029377c0269c27afc7b27264e93663cfd83c1b
karshilsheth <karshilsheth@gmail.com>
2020-05-07 22:28:56 +0530

    Updated the code Challenge Practice!!

---
+++ JavaPracticeSolving/src/com/datatypes/IsAInheritance.java
@@ -0,0 +1,22 @@
+package com.datatypes;
+
+import java.math.BigInteger;
+
+public class IsAInheritance extends karshil {
+	
+	int bonu=balance+salary;
+	int c=89;
+		public static void main(String args[])
+	{ 
+		IsAInheritance obj=new IsAInheritance();
+		System.out.println(obj.salary);
+		System.out.println(obj.bonu);
+		
+	}
+	
+}
+class karshil{
+	 int salary=1000;
+	 int balance=4000;
+
+}
